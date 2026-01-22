0b17c1a5c0318514d09c942a12ffa307baed2c1b
Boshuai Li <liboshuai01@gmail.com>
2026-01-22 13:26:03 +0800

    feat(tmp): FutureUtils新增combineForAll功能

--- tmp/src/main/java/cn/liboshuai/scratch/tmp/FutureUtils.java
+++ tmp/src/main/java/cn/liboshuai/scratch/tmp/FutureUtils.java
@@ -1,6 +1,8 @@
 package cn.liboshuai.scratch.tmp;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -53,8 +55,56 @@ public class FutureUtils {
         }
     }
 
+    public static final class ResultConjunctFuture<T> extends ConjunctFuture<Collection<T>> {
+
+        private final int numTotal;
+        private final AtomicInteger numCompleted = new AtomicInteger(0);
+        private final T[] results;
+
+        public void handleCompletedFuture(int index, T value, Throwable throwable) {
+            if (throwable == null) {
+                results[index] = value;
+                if (numTotal == numCompleted.incrementAndGet()) {
+                    complete(Arrays.asList(results));
+                }
+            } else {
+                completeExceptionally(throwable);
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        public ResultConjunctFuture(Collection<? extends CompletableFuture<T>> futures) {
+            this.numTotal = futures.size();
+            results = (T[]) new Object[numTotal];
+            if (futures.isEmpty()) {
+                complete(Collections.EMPTY_LIST);
+            } else {
+                int count = 0;
+                for (CompletableFuture<T> future : futures) {
+                    int index = count++;
+                    future.whenComplete((T value, Throwable throwable) -> this.handleCompletedFuture(index, value, throwable));
+                }
+            }
+        }
+
+        @Override
+        public int getNumFuturesTotal() {
+            return 0;
+        }
+
+        @Override
+        public int getNumFuturesCompleted() {
+            return 0;
+        }
+    }
+
     public static ConjunctFuture<Void> waitForAll(Collection<? extends CompletableFuture<?>> futures) {
         checkNotNull(futures, "futures");
         return new WaitingConjunctFuture(futures);
+    }
+
+    public static <T> ConjunctFuture<Collection<T>> combineForAll(Collection<? extends CompletableFuture<T>> futures) {
+        checkNotNull(futures, "futures");
+        return new ResultConjunctFuture<>(futures);
     }
 }
