8079e831b50effad1381d797b6c1c415323488d7
Boshuai Li <liboshuai01@gmail.com>
2026-01-25 09:14:39 +0800

    feat(tmp): FutureUtils中新增unsupportedOperationFuture方法

--- flink/flink-mini/src/main/java/cn/liboshuai/scratch/flink/mini/util/concurrent/FutureUtils.java
+++ flink/flink-mini/src/main/java/cn/liboshuai/scratch/flink/mini/util/concurrent/FutureUtils.java
@@ -9,8 +9,23 @@ public class FutureUtils {
 
     private static final CompletableFuture<Void> COMPLETED_VOID_FUTURE = CompletableFuture.completedFuture(null);
 
+    private static final CompletableFuture<?> UNSUPPORTED_OPERATION_FUTURE = completedExceptionally(
+            new UnsupportedOperationException("The method is unsupported.")
+    );
+
     public static CompletableFuture<Void> completedVoidFuture() {
         return COMPLETED_VOID_FUTURE;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> CompletableFuture<T> unsupportedOperationFuture() {
+        return (CompletableFuture<T>) UNSUPPORTED_OPERATION_FUTURE;
+    }
+
+    public static <T> CompletableFuture<T> completedExceptionally(Throwable throwable) {
+        CompletableFuture<T> completableFuture = new CompletableFuture<>();
+        completableFuture.completeExceptionally(throwable);
+        return completableFuture;
     }
 
 }
