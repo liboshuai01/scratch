e690404718040bb256e134f91b0e516a7d7bb26b
Boshuai Li <liboshuai01@gmail.com>
2026-01-28 13:35:51 +0800

    feat(flink-mini): ExceptionUtils中新增stringifyException方法

---
+++ flink/flink-mini/src/main/java/cn/liboshuai/scratch/flink/mini/util/ExceptionUtils.java
@@ -0,0 +1,24 @@
+package cn.liboshuai.scratch.flink.mini.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public final class ExceptionUtils {
+
+    private static final String STRINGIFIED_NULL_EXCEPTION = "(null)";
+
+    public static String stringifyException(final Throwable throwable) {
+        if (throwable == null) {
+            return STRINGIFIED_NULL_EXCEPTION;
+        }
+        try {
+            StringWriter stringWriter = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(stringWriter);
+            throwable.printStackTrace(printWriter);
+            printWriter.close();
+            return stringWriter.toString();
+        } catch (Throwable e) {
+            return e.getClass().getName() + " (error while printing stack trace)";
+        }
+    }
+}
